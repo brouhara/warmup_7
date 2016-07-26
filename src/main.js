bf07ca560be8eb45f9cf5910f59ea4a1af276c94
brouhara <adam.brouhard@gmail.com>
2016-07-26 10:42:24 -0600

    init

--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
 $(document).ready(function () {
 
-	console.log('test'); // sanity check
+  console.log('test'); // sanity check
 
-	$('form').on('submit', function (event) {
+  $('form').on('submit', function (event) {
 
-		event.preventDefault();
-		console.log('this is working');
+    event.preventDefault();
+    console.log('this is working');
 
-		 var input = $('#search-input').val();
+    var input = $('#search-input').val();
 
-		  $.ajax({
-			type: "GET",
-			url: "http://www.omdbapi.com/?t=" + input,
-			data: { name: pokeName, number: pokeNum },
-			dataType: "dataType",
-			success: function (response) {
-				$('#alert').val('Success!');
-			}
-		  }).done( function( results ) {
-			console.log( 'Movie Response:', results) ;
-		  });
-	});
+    $.ajax({
+      type: 'GET',
+      url: 'http://www.omdbapi.com/?t=' + input,
+      data: { name: pokeName, number: pokeNum },
+      dataType: "dataType",
+      success: function (response) {
+        $('#alert').val('Success!');
+      }
+    }).done(function (results) {
+      console.log('Movie Response:', results);
+    });
+  });
 });
