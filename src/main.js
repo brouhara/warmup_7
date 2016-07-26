1027550af6340459fb49b614d5f2113d9090aed3
brouhara <adam.brouhard@gmail.com>
2016-07-26 11:12:22 -0600

    added default search value and ajax Request for OMDB

--- src/main.js
+++ src/main.js
@@ -1,24 +1,39 @@
+
+var input;
+
+var OMDBRequest = {
+  type: 'GET',
+  url: 'http://www.omdbapi.com/',
+  data: { t: input },
+  dataType: 'dataType',
+  success: function (response) {
+    $('#alert').val('Success!');
+  }
+}
+
+function ajaxRequest(request) {
+  $.ajax(request).done(function(result) { ajaxResponse(result) });
+}
+
+function ajaxResponse(result) {
+     console.log('Ajax Response:');
+}
+
+function ajaxMovieRequest() {
+  OMDBRequest.data.t = $('#search-input').val();
+  ajaxRequest(OMDBRequest);
+}
+
 $(document).ready(function () {
 
   console.log('test'); // sanity check
 
   $('form').on('submit', function (event) {
-
     event.preventDefault();
     console.log('this is working');
 
-    var input = $('#search-input').val();
-
-    $.ajax({
-      type: 'GET',
-      url: 'http://www.omdbapi.com/?t=' + input,
-      data: { name: pokeName, number: pokeNum },
-      dataType: 'dataType',
-      success: function (response) {
-        $('#alert').val('Success!');
-      }
-    }).done(function (results) {
-      console.log('Movie Response:', results);
-    });
+    ajaxMovieRequest();
   });
-});
+
+})
+
