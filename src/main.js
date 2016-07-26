7afa9425177213db3c39b97c0804ff067722f347
brouhara <adam.brouhard@gmail.com>
2016-07-26 10:19:06 -0600

    Added main.js script tag

---
+++ src/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+	console.log('test'); // sanity check
+
+	$('form').on('submit', function (event) {
+
+		event.preventDefault();
+		console.log('this is working');
+
+		 var input = $('#search-input').val();
+
+		  $.ajax({
+			type: "GET",
+			url: "http://www.omdbapi.com/?t=" + input,
+			data: { name: pokeName, number: pokeNum },
+			dataType: "dataType",
+			success: function (response) {
+				$('#alert').val('Success!');
+			}
+		  }).done( function( results ) {
+			console.log( 'Movie Response:', results) ;
+		  });
+		// var formFields = $(this).serialize();
+		// console.log(formFields);
+	});
+});
